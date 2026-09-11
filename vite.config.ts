89500ca9ab28ed6769ea69bd968093d5117412f4
hisuie08 <keisyu0825@gmail.com>
2026-09-11 03:07:01 +0000

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { fileURLToPath } from "node:url";
 const rootDir = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "./",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
